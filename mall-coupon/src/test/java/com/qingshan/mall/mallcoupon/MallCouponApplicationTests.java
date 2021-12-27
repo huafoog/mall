2d0f7183838370704bd23d76a04a9c5d9b4da5fe
Dempasy Dave <zhangyaxing521@qq.com>
2021-12-27 16:17:09 +0800

    项目初始化

---
+++ mall-coupon/src/test/java/com/qingshan/mall/mallcoupon/MallCouponApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qingshan.mall.mallcoupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallCouponApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
